1225f19231dc3fe22e1894d4fdfdb6d0db62da61
GRADIENT <archivemonologue@gmail.com>
2025-05-22 16:56:27 -0400

    WORKS

--- coxlambertactualACTUAL.m
+++ coxlambertactualACTUAL.m
@@ -32,7 +32,23 @@ temp_init = 300; %K
 beta = 50; %K/s
 time = @(x) (x-temp_init)/beta;
 
-new_x_0p4_time = time(new_x_0p4);
+figure(2); clf;
+hold on;
+plot(time(new_x_0p4), new_y_0p4, 'o-');
+plot(time(new_x_0p8), new_y_0p8, 'o-');
+plot(time(new_x_1p2), new_y_1p2, 'o-');
+plot(time(new_x_1p6), new_y_1p6, 'o-');
+plot(time(new_x_2p0), new_y_2p0, 'o-');
+plot(time(new_x_2p8), new_y_2p8, 'o-');
+plot(time(new_x_4p0), new_y_4p0, 'o-');
+plot(time(new_x_8p0), new_y_8p0, 'o-');
+
+set(gca, 'ColorOrder', cmap, 'NextPlot', 'replacechildren');
+legend('0.4', '0.8', '1.2', '1.6', '2.0', '2.8', '4.0', '8.0');
+xlabel('Time (s)');
+ylabel('Spectrometer Signal');
+title('Desorption Traces, Signal vs Time')
+hold off;
 
 
 %% Determine coverage - dosage relationship. Copied from the first 'actual'.
